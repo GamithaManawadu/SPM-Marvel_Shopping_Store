580a072a6b94aae938e324c43f58665c5bddb902
Sachindu <sachindugimhana0@gmail.com>
2021-08-27 10:46:39 +0530

    sweet alert added

--- frontend/src/pages/Admin/Home/Adminhome.jsx
+++ frontend/src/pages/Admin/Home/Adminhome.jsx
@@ -1,28 +1,26 @@
-import React from 'react'
-import Chart from '../../../components/AdminDashboard/chart/Chart';
-import FeatureInfo from '../../../components/AdminDashboard/featureInfo/FeatureInfo'
-import Features from '../../../components/AdminDashboard/featureInfo/Features';
-import Table from '../../../components/AdminDashboard/table/Table';
+import React from "react";
+import Chart from "../../../components/AdminDashboard/chart/Chart";
+import FeatureInfo from "../../../components/AdminDashboard/featureInfo/FeatureInfo";
+import Features from "../../../components/AdminDashboard/featureInfo/Features";
+import Table from "../../../components/AdminDashboard/table/Table";
 import AdminDashboard from "../../../components/AdminDashboard/AdminDashboard";
-
 
 import "./home.css";
 
 export default function home() {
-    return (
-            <>
-        <div className="editor">
-			
-			<AdminDashboard/>
-            </div>
-        <div className="home">
-            <FeatureInfo />
-            <div className="homeWidget">
-                <Chart />
-                <Table />
-            </div>
-            <Features />
+  return (
+    <>
+      <div className="editor">
+        <AdminDashboard />
+      </div>
+      <div className="home">
+        <FeatureInfo />
+        <div className="homeWidget">
+          <Chart />
+          <Table />
         </div>
-        </>
-    )
+        <Features />
+      </div>
+    </>
+  );
 }
