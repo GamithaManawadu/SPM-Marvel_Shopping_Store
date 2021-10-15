d5dd23789b5e074f09395fe64d591ea1a6ddbb2e
Gamitha <gamithamanawadu26@gmail.com>
2021-10-15 14:18:48 +0530

    updated

--- frontend/storeManager/client/src/components/views/ProductList/setErrors.js
+++
@@ -1,6 +0,0 @@
-export const setErrors = (title, price) => {
-    let errors = {};
-    errors.title = title ? "" : "Title is required!";
-    errors.price = price ? "" : "Price is required!";
-    return errors;
-  };
